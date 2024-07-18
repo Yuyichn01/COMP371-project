dd13e2532437f0a4d87bec2bf1dbfca060984f91
LYNX1239038120 <51261630+Yuyichn01@users.noreply.github.com>
2024-07-17 22:51:10 -0400

    Add files via upload

---
+++ driver.h
@@ -0,0 +1,16 @@
+//
+// Created by 余翊 on 2024-07-15.
+//
+
+#ifndef COMP371_PROJECT_DRIVER_H
+#define COMP371_PROJECT_DRIVER_H
+
+namespace std {
+
+    class driver {
+
+    };
+
+} // std
+
+#endif //COMP371_PROJECT_DRIVER_H
